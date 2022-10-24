e5ab0aa0abdb46c2a7a02754645dc160a4a9fe78
Remix Workspace <>
2022-10-24 21:40:10 +0900

    lesson4

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,20 +3,58 @@ pragma solidity ^0.8.8;
 
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50 * 1e18;
+    uint256 public constant MININMUM_USD = 50 * 1e18;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public immutable i_owner;
+
+    constructor(){
+        i_owner = msg.sender;
+    }
+
     function fund() public payable{
 
-        require(msg.value >= minimumUsd, "Didnt send enough");
+        require(PriceConverter.getConversionRate(msg.value) >= MININMUM_USD, "Didn't send enough");
         // 18 decimals
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner {
+        // require(msg.sender == owner, "Sender is not owner");
+        if(msg.sender != i_owner) { revert NotOwner(); }
+        _;
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 
 }
