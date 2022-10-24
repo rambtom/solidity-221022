ca4c71993e81cff23c45b75e19b5047f8e2d292b
Remix Workspace <>
2022-10-24 14:06:37 +0900

    1024

---
+++ contracts/FundMe.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
+    function fund() public payable{
+
+        require(msg.value >= minimumUsd, "Didnt send enough");
+        // 18 decimals
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+}
